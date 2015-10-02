c8577b2939743f4d3456befff5725a1157317ae7
opsigor <igorpsant95@gmail.com>
2015-10-01 23:02:24 -0300

    fix

--- sv.js
+++ sv.js
@@ -1,4 +1,4 @@
 'use strict';
 let svv = require('./lib/index');
-// svv(['rId6PKlDXeU', 'sXzDu071RdQ','1N8BYNMMjqU'], './musics/');
-// svv('ZVMIk3xYaYo','./musics/')
+svv(['rId6PKlDXeU', 'sXzDu071RdQ','1N8BYNMMjqU'], './musics/');
+// svv('-OVHnWyDmdw','./musics/')
