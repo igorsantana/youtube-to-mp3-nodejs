769a9e7c02a0fd4b870b765656d304286eabadc2
opsigor <igorpsant95@gmail.com>
2015-10-03 17:55:59 -0300

    Now it has callback when its finished

--- sv.js
+++ sv.js
@@ -2,5 +2,5 @@
 let svv = require('./lib/index');
 // svv(['rId6PKlDXeU', 'sXzDu071RdQ','1N8BYNMMjqU'], './musics/');
 // svv('zvCBSSwgtg4', './musics/')
-// svv.playlistDownload('PLnaSPks1YwbCidMZO7rSdKF56L5FJu7rw','./musics/')
+svv.playlistDownload('PLnaSPks1YwbDsiOWbuOTi-B2yezM0t1tW', './musics/', (data) => console.log(data));
 console.log(svv);
