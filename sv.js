d422daaa0647ee957277c313097a8c3fa1eaa410
opsigor <igorpsant95@gmail.com>
2015-10-04 02:12:00 -0300

    Now working with events, and now it has to be instantiated

--- sv.js
+++ sv.js
@@ -1,6 +1,10 @@
 'use strict';
-let svv = require('./lib/index');
-// svv(['rId6PKlDXeU', 'sXzDu071RdQ','1N8BYNMMjqU'], './musics/');
-// svv('zvCBSSwgtg4', './musics/')
-svv.playlistDownload('PLnaSPks1YwbDsiOWbuOTi-B2yezM0t1tW', './musics/', (data) => console.log(data));
-console.log(svv);
+let Yt = require('./lib/index'),
+  teste = new Yt();
+// teste.videoDownload(['rId6PKlDXeU', 'sXzDu071RdQ', '1N8BYNMMjqU'], './musics/', (data) => console.log(data));
+
+
+teste
+  .on('download', (title) => console.log('Download: ' + title))
+  .on('conversion', (title) => console.log('Conversion: ' + title))
+  .on('finished', () => console.log('Everything is over now'));
