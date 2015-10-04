51b4bc4d05ba71fb95cb9d67694d5fb5d11dc98c
opsigor <igorpsant95@gmail.com>
2015-10-04 02:48:54 -0300

    fix

--- sv.js
+++ sv.js
@@ -1,10 +1,11 @@
 'use strict';
 let Yt = require('./lib/index'),
   teste = new Yt();
-// teste.videoDownload(['rId6PKlDXeU', 'sXzDu071RdQ', '1N8BYNMMjqU'], './musics/', (data) => console.log(data));
+teste.playlistDownload('PLnaSPks1YwbBzxhos17Hg758Y_EB8Nzbk', './musics/');
 
 
 teste
   .on('download', (title) => console.log('Download: ' + title))
   .on('conversion', (title) => console.log('Conversion: ' + title))
+  .on('error',(err)=> console.log(err))
   .on('finished', () => console.log('Everything is over now'));
